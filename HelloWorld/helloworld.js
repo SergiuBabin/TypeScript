7e2780a585bd1152ed1cc9c9b14f6b42494df61b
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2021-05-06 14:55:33 +0300

    First look

---
+++ HelloWorld/helloworld.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Sergiu";
+var lastName = 'Babin';
+grade = 1000;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
---
+++ HelloWorld/helloworld.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Sergiu"
+let lastName: string = 'Babin'
+
+grade = 1000;
+
+console.log(found)
+console.log("The grade is " + grade);
+console.log(`Hi ${firstName} ${lastName}`);
